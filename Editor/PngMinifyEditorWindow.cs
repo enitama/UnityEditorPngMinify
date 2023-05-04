7efb7786d76a2758a4f593c22b7713baa6db9dab
eni <129673786+enitama@users.noreply.github.com>
2023-05-04 17:29:15 +0900

    Add Japanese localization

--- Editor/PngMinifyEditorWindow.cs
+++ Editor/PngMinifyEditorWindow.cs
@@ -26,6 +26,47 @@ namespace io.github.enitama.pngminify.Editor
         private StringBuilder _outputBuilder = new StringBuilder();
         private Vector2 _outputScrollPosition;
         private bool _isProcessRunning = false;
+        private int _selectedLanguage = 0;
+
+        // TODO: Adopt a better localization method.
+        private Dictionary<string, string> SupportedLanguages = new Dictionary<string, string> { { "en", "English" }, { "ja", "日本語" } };
+        private Dictionary<string, Dictionary<string, string>> Localizations = new Dictionary<string, Dictionary<string, string>>
+        {
+            {
+                "en", new Dictionary<string, string>
+                {
+                    { "lbl.pngquantpath", "Path to pngquant:" },
+                    { "lbl.targetfolder", "Target folder:" },
+                    { "lbl.imagequality", "Quality:" },
+                    { "lbl.running", "Running" },
+                    { "msg.pngquantblank", "Please specify path to pngquant." },
+                    { "msg.pngquantincompatbile", "Couldn't find compatible pngquant!" },
+                    { "msg.reduced", "Reduced {0} to {1} ({2}% savings)" },
+                    { "btn.execute", "Execute" },
+                    { "log.running", "[Running]" },
+                    { "log.exited", "[Exited]" },
+                    { "log.exitcode", "Exit code: {0}" },
+                }
+            },
+            {
+                "ja", new Dictionary<string, string>
+                {
+                    { "lbl.pngquantpath", "pngquantのパス:" },
+                    { "lbl.targetfolder", "ターゲットフォルダー:" },
+                    { "lbl.imagequality", "画質:" },
+                    { "lbl.running", "実行中" },
+                    { "msg.pngquantblank", "pngquantのパスを指定してください。" },
+                    { "msg.pngquantincompatbile", "互換性のあるpngquantが見つかりませんでした！" },
+                    { "msg.reduced", "{0}を{1}に縮小しました（{2}%の節約）" },
+                    { "btn.execute", "実行" },
+                    { "log.running", "[実行中]" },
+                    { "log.exited", "[終了]" },
+                    { "log.exitcode", "終了コード: {0}" },
+                }
+            }
+        };
+        private string GetLocalizedString(string name, params object[] args) => GetLocalizedStringForLang(SupportedLanguages.Keys.ElementAt(_selectedLanguage), name, args);
+        private string GetLocalizedStringForLang(string code, string name, params object[] args) => string.Format(Localizations[code][name], args);
 
         [MenuItem("Tools/PngMinify")]
         public static void ShowWindow()
@@ -35,12 +76,15 @@ namespace io.github.enitama.pngminify.Editor
 
         private void OnGUI()
         {
+            EditorGUILayout.BeginHorizontal();
             GUILayout.Label("PngMinify 0.0.2", EditorStyles.boldLabel);
+            _selectedLanguage = EditorGUILayout.Popup(_selectedLanguage, SupportedLanguages.Values.ToArray(), GUILayout.Width(100));
+            EditorGUILayout.EndHorizontal();
 
             GUILayout.Space(10);
 
             EditorGUILayout.BeginHorizontal();
-            GUILayout.Label("Path to pngquant:", GUILayout.Width(100));
+            GUILayout.Label(GetLocalizedString("lbl.pngquantpath"), GUILayout.Width(100));
             if (GUILayout.Button("Browse"))
             {
                 _pngquantPath = EditorUtility.OpenFilePanel("Select File", "", "");
@@ -50,14 +94,14 @@ namespace io.github.enitama.pngminify.Editor
             GUILayout.Space(10);
 
             EditorGUILayout.BeginHorizontal();
-            GUILayout.Label("Target folder:", GUILayout.Width(100));
+            GUILayout.Label(GetLocalizedString("lbl.targetfolder"), GUILayout.Width(100));
             _targetFolder = EditorGUILayout.ObjectField(_targetFolder, typeof(DefaultAsset), false) as DefaultAsset;
             EditorGUILayout.EndHorizontal();
 
             GUILayout.Space(10);
 
             EditorGUILayout.BeginHorizontal();
-            GUILayout.Label("Quality:", GUILayout.Width(100));
+            GUILayout.Label(GetLocalizedString("lbl.imagequality"), GUILayout.Width(100));
             _qualityValue = EditorGUILayout.IntSlider(_qualityValue, 0, 100);
             EditorGUILayout.EndHorizontal();
 
@@ -65,7 +109,7 @@ namespace io.github.enitama.pngminify.Editor
 
             if (!_isProcessRunning)
             {
-                if (GUILayout.Button("Execute"))
+                if (GUILayout.Button(GetLocalizedString("btn.execute")))
                 {
                     if (!EnsurePngquant(_pngquantPath))
                     {
@@ -80,7 +124,7 @@ namespace io.github.enitama.pngminify.Editor
             }
             else
             {
-                GUILayout.Label("Running...");
+                GUILayout.Label(GetLocalizedString("lbl.running"));
             }
 
             GUILayout.Space(10);
@@ -101,13 +145,13 @@ namespace io.github.enitama.pngminify.Editor
             Repaint();
             foreach (var image in images)
             {
-                _outputBuilder.AppendLine("[Running]");
+                _outputBuilder.AppendLine(GetLocalizedString("log.running"));
                 int exitCode = await RunPngquant(path, image, _qualityValue, output =>
                 {
                     _outputBuilder.AppendLine(output);
                     Repaint();
                 });
-                _outputBuilder.AppendLine("[Finished] Error code: " + exitCode);
+                _outputBuilder.AppendLine(GetLocalizedString("log.exited") + GetLocalizedString("log.exitcode", exitCode));
                 string newPath = Path.Combine(Path.GetDirectoryName(image.Path), Path.GetFileNameWithoutExtension(image.Path) + "-fs8" + Path.GetExtension(image.Path));
                 var newFileInfo = new FileInfo(newPath);
                 if (newFileInfo.Exists)
@@ -123,7 +167,8 @@ namespace io.github.enitama.pngminify.Editor
             // Use MB not MiB.
             double mbBefore = images.Aggregate(0L, (acc, x) => acc + x.BytesBefore) / 1000.0 / 1000.0;
             double mbAfter = images.Aggregate(0L, (acc, x) => acc + (x.BytesAfter > 0 ? x.BytesAfter : x.BytesBefore)) / 1000.0 / 1000.0;
-            _outputBuilder.AppendLine($"Reduced {mbBefore} MB to {mbAfter} MB");
+            double saved = (mbAfter - mbBefore) / mbBefore;
+            _outputBuilder.AppendLine(GetLocalizedString("msg.reduced", $"{mbBefore} MB", $"{mbAfter} MB", saved * 100));
             _isProcessRunning = false;
             Repaint();
         }
@@ -155,11 +200,11 @@ namespace io.github.enitama.pngminify.Editor
             return tcs.Task;
         }
 
-        private static bool EnsurePngquant(string path)
+        private bool EnsurePngquant(string path)
         {
             if (string.IsNullOrEmpty(path))
             {
-                Debug.Log("Path is blank!");
+                EditorUtility.DisplayDialog("Error", GetLocalizedString("msg.pngquantblank"), "OK");
                 return false;
             }
             var sb = new StringBuilder();
@@ -175,7 +220,7 @@ namespace io.github.enitama.pngminify.Editor
             process.WaitForExit();
             if (!stderr.Contains("pngquant, 2."))
             {
-                EditorUtility.DisplayDialog("Error", "Compatible pngquant not found", "OK");
+                EditorUtility.DisplayDialog("Error", GetLocalizedString("msg.pngquantincompatible"), "OK");
                 return false;
             }
             return true;
